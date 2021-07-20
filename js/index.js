d203fbb2fab9c4f013f3ffca6234f1b75334d22c
whoismilo <77068296+whoismilo@users.noreply.github.com>
2021-07-19 20:30:30 -0700

    add Under Construction
    update and add construction pages. create Contact fields

--- js/index.js
+++ js/index.js
@@ -3,3 +3,35 @@
     $("#dropDown").slideToggle();
   });
   
+  $('.dlresumebutton').on("click", function() {
+    alert("Resume downloaded");
+});
+
+$('.myworkimg').hover(function(){
+    $(this).width(1.03*$(this).width());
+    $(this).height(1.03*$(this).height());
+}, function(){
+    $(this).width(0.97*$(this).width());
+    $(this).height(.97*$(this).height());
+
+    console.log("mouse leave");
+});
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
